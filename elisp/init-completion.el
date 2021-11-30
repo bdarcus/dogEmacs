08c1e7e2ca66631a015804426d5c759b2485b792
Bruce D'Arcus <bdarcus@gmail.com>
2021-11-30 14:11:49 -0500

    initial start of adaption

--- elisp/init-completion.el
+++ elisp/init-completion.el
@@ -2,15 +2,10 @@
 
 (straight-use-package 'vertico)
 (straight-use-package 'orderless)
-(straight-use-package 'company)
+(straight-use-package 'corfu)
 (straight-use-package 'rg)
-(straight-use-package 'prescient)
+;(straight-use-package 'prescient)
 (straight-use-package 'yasnippet)
-
-(defun +complete ()
-  (interactive)
-  (or (yas/expand)
-      (company-indent-or-complete-common nil)))
 
 ;;; yasnippet
 
@@ -30,61 +25,8 @@
   (define-key yas-keymap (kbd "RET") 'yas-next-field-or-maybe-expand)
   (define-key yas-keymap (kbd "S-<return>") 'yas-prev-field))
 
-;;; company
+;;; corfu
 
-(setq
- company-tng-auto-configure nil
- company-frontends '(company-tng-frontend
-                     company-pseudo-tooltip-frontend
-                     company-echo-metadata-frontend)
- company-begin-commands '(self-insert-command)
- company-idle-delay 0
- company-tooltip-limit 10
- company-tooltip-align-annotations t
- company-tooltip-width-grow-only t
- company-tooltip-idle-delay 0.4
- company-minimum-prefix-length 3
- company-dabbrev-downcase nil
- company-abort-manual-when-too-short t
- company-require-match nil
- company-global-modes '(not dired-mode dired-sidebar-mode)
- company-tooltip-margin 0
- company-format-margin-function nil)
-
-(autoload #'company-mode "company")
-
-(add-hook 'prog-mode-hook 'company-mode)
-(add-hook 'conf-mode-hook 'company-mode)
-(add-hook 'eshell-mode-hook 'company-mode)
-
-(with-eval-after-load "company"
-
-  (require 'company-tng)
-  (require 'company-template)
-
-  (add-hook 'company-mode-hook 'company-tng-mode)
-
-  (define-advice company-capf--candidates (:around (func &rest args))
-    "Completion styles setup for company"
-    (let ((completion-styles '(basic partial-completion)))
-      (apply func args)))
-
-  (define-key company-mode-map [tab] '+complete)
-  (define-key company-mode-map (kbd "TAB") '+complete)
-  (define-key company-active-map [tab] 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-  (define-key company-active-map [escape] nil)
-  (define-key company-active-map [return] nil)
-  (define-key company-active-map (kbd "RET") nil)
-  (define-key company-active-map (kbd "SPC") nil)
-  (define-key company-active-map (kbd "SPC") nil)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-
-  (define-key company-template-nav-map (kbd "RET") 'company-template-forward-field)
-  (define-key company-template-nav-map [return] 'company-template-forward-field)
-  (define-key company-template-nav-map (kbd "TAB") nil)
-  (define-key company-template-nav-map [tab] nil))
 
 ;; vertico
 
